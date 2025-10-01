3370ce78bab9a1d591f974bce6ec44d122ab1189
Charles Ferguson <gerph@gerph.org>
2025-10-02 00:38:31 +0100

    Update zero-init code to check that we have enough memory.
    Before we try to write over lots of memory, we now check whether we
    have enough space in the application slot. If we don't we raise an
    error, rather than trying to initialise it and then crashing when
    we hit the unallocated memory (or possibly overwriting the app at
    the top of the slot - if we ever actually do things that way).

--- clib/start.s
+++ clib/start.s
@@ -13,11 +13,16 @@ _aif64_entry:
         BL      __RelocCode     // Relocation code
         BL      _zeroinit       // Zero initialisation
         BL      _start
+_aif64_exit:
         MOV     x10, #0x11      // OS_Exit
         SVC     #0
 
 _zeroinit:
 .type _zeroinit, @function
+        MOV     x10, #OS_GetEnv
+        SVC     #0
+        MOV     x7, x1                  // RAM limit (will be stack pointer)
+
         SUB     x0, lr, #0x100 + 0x8    // Start of the application space
         LDR     w3, [x0, #0x14] // code size
         LDR     w4, [x0, #0x18] // data size
@@ -27,6 +32,26 @@ _zeroinit:
         MOV     x5, #0
         MOV     x6, #0
 
+// check that we're actually able to start by checking the application space size
+        SUB     x7, x7, #2048           // Minimum stack size we expect
+        ADD     x2, x1, x4              // Zero-init limit
+        CMP     x7, x2
+        BHI     _zeroinit_okmemory
+// We do not have enough memory for the zero init'd data.
+// This probably should have been checked by the AIF loader, but if for any
+// reason it isn't we can now fail.
+        ADR     x0, _zeroinit_memoryerror
+        MOV     x10, #0x2B              // OS_GenerateError
+        SVC     #0
+        B       _aif64_exit
+
+_zeroinit_memoryerror:
+.word   0x1B000000
+.ascii "Not enough memory\0"
+
+.balign 4
+
+_zeroinit_okmemory:
         CMP     x4, #128
         BLO     _zeroinit_fastdone
 _zeroinit_fastloop:
