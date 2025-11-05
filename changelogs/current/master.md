d2241cf15565d872b380865b6f2c400c7a47a52e
Charles Ferguson <gerph@gerph.org>
2025-11-05 22:31:38 +0000

    Added `mktemp` function.
    We now support the `mktemp` function to get a temporary file with a
    randomised suffix.

--- changelogs/current/master.md
+++ changelogs/current/master.md
@@ -2,7 +2,7 @@
 - 
 
 ### Added
-- 
+- stdlib: `mktemp` now implemented.
 
 ### Changed
 - linking: `riscos64-addsignatures` now trims the `.hidden ` labels from hidden symbols.
--- clib/fs/fs-tmpnam.c
+++ clib/fs/fs-tmpnam.c
@@ -49,22 +49,65 @@ static char *_sys_tmpnam_(char *name, int sig)
 
 
 static char _tmp_file_name[L_tmpnam] = "";
+static int counter = 0;
+
+#define getsig() (clock() ^ counter++)
 
 char *tmpnam(char *name)
 {
-    static int counter = 0;
+    int attempts = 10;
     if (name == NULL)
         name = _tmp_file_name;
 
     do {
-      int sig = clock() ^ counter++;
+      int sig = getsig();
       /* JRF: Return NULL if we cannot generate a temporary filename. */
       if (!_sys_tmpnam_(name, sig))
       {
         errno = ENOENT;
         return NULL;
       }
-    } while (_kernel_osfile(17, name, NULL) != 0);
+      attempts--;
+    } while (_kernel_osfile(17, name, NULL) != 0 || attempts == 0);
+
+    if (attempts == 0)
+    {
+        /* Failure */
+        errno = ENOENT;
+        return NULL; /* Many attempts made and we were not successful */
+    }
 
     return name;
+}
+
+
+char *mktemp(char *template)
+{
+    char *marker = template + strlen(template) - 1;
+    int attempts = 10;
+    if (marker <= template)
+        return NULL;
+    if (*marker != 'X')
+        return NULL; /* There were no 'X' characters present */
+    while (marker > template && *marker == 'X')
+        marker--;
+
+    while (1)
+    {
+        int sig = getsig();
+        const char dict[33] = "ABCDEFGHIJKLMNOPQRSTYUVWXYZ" // 26
+                              "123456"; // 32
+        char *p = marker;
+        int i=0;
+        while (*p != '\0')
+        {
+            int c = dict[(sig>>i) & 31];
+            i += 5;
+            if (i >= 31 - 5)
+                i -= 25;
+            *p++ = c;
+        }
+        attempts --;
+    } while (_kernel_osfile(17, template, NULL) != 0 || attempts == 0);
+    return template;
 }
